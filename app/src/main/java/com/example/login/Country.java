f406fbf0231ffaf4b4403514db0981176eb7ab46
INFO PC <douaa.bousseboua@univ-constantine2.dz>
2023-11-08 15:32:34 +0100

    adapter

---
+++ app/src/main/java/com/example/login/Country.java
@@ -0,0 +1,11 @@
+package com.example.login;
+
+public class Country {
+    int flagResourceId;
+    String name;
+
+    public Country(int flagResourceId, String name){
+        this.flagResourceId = flagResourceId;
+        this.name = name;
+    }
+}
